14a894336dc8a72dfc6cd2e458f32b751af7e51f
Antonio Lirio <96416888+Tonilirio@users.noreply.github.com>
2025-01-13 22:55:15 +0100

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,11 +1,8 @@
 export default async function decorate(block) {
-  let row = block.firstElementChild;
+  const row = block.firstElementChild;
   const bg = row.querySelector('picture');
+  const bgP = bg.closest('p');
   block.append(bg);
-  row.remove();
-  const bgP = block.closest('p');
   if (bgP) bgP.remove();
-
-  row = block.firstElementChild;
   row.classList.add('hero-body');
 }
